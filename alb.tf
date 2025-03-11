599d49cac9a54779288a2d8231698acb8443eba6
nitishdadapuram10 <62438926+nitishdadapuram10@users.noreply.github.com>
2025-03-11 17:42:21 -0500

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "p2_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.p2_alb_sg.id]
-  subnets            = [aws_subnet.p2_public_subnet.id]
+  subnets            = [aws_subnet.p2_public_subnet.id,aws_subnet_p2_public_subnet_2.id]
 
  
 
