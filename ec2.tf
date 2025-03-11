432c47ccc95e4c041fe2cd9074feb13173a8c6f2
nitishdadapuram10 <62438926+nitishdadapuram10@users.noreply.github.com>
2025-03-11 18:39:47 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "p2_ec2" {
   user_data = <<-EOF
               #!/bin/bash
               sudo yum install httpd -y
-              echo "Welcome to Cloud P2" > /var/www/html/index.html
+              echo "Welcome to Cloud P2" | sudo tee /var/www/html/index.html
               sudo systemctl start httpd
               sudo systemctl enable httpd
               EOF
