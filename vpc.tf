f6394a94e361e8e80ec41dd916df5d207f3a95d0
nitishdadapuram10 <62438926+nitishdadapuram10@users.noreply.github.com>
2025-03-11 17:39:52 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "p2_public_subnet" {
 
 resource "aws_subnet" "p2_public_subnet_2" {
   vpc_id     = aws_vpc.p2_vpc.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1b"
   map_public_ip_on_launch = true
   tags = {
