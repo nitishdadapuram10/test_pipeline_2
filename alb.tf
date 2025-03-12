953554e6e5d4fae672076fecf81c21527088e066
nitishdadapuram10 <62438926+nitishdadapuram10@users.noreply.github.com>
2025-03-11 20:28:10 -0500

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -20,21 +20,27 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.p2_ec2_target_group.arn
+    target_group_arn = aws_lb_target_group.p2_ec2_target.arn
   }
 }
 
-resource "aws_lb_target_group" "p2_ec2_target_group" {
+resource "aws_lb_target_group" "p2_ec2_target" {
   name     = "ec2-target-group"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.p2_vpc.id
-
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
 
 }
 
 resource "aws_lb_target_group_attachment" "attachment" {
-  target_group_arn = aws_lb_target_group.p2_ec2_target_group.arn
+  target_group_arn = aws_lb_target_group.p2_ec2_target.arn
   target_id        = aws_instance.p2_ec2.id
   port             = 80
 }
